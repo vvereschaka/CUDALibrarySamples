46733d0358d8a20468a8bf59b894b364a630c24d
Vladimir Vereschaka <vvereschaka@nvidia.com>
2023-01-04 17:24:48 -0800

    cuRAND examples refactoring in order to the batch builds.

--- cuRAND/cmake/curand_example.cmake
+++ cuRAND/cmake/curand_example.cmake
@@ -1,7 +1,7 @@
-# 
+#
 # Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
 #
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
 # met:
@@ -13,10 +13,10 @@
 #  - Neither the name(s) of the copyright holder(s) nor the names of its
 #    contributors may be used to endorse or promote products derived
 #    from this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
 # A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
 # HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
 # SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
@@ -25,33 +25,69 @@
 # THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# 
+#
+
+# ##########################################
+# curand_examples build mode
+# ##########################################
+
+if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+    message(STATUS "Setting build type to 'Release' as none was specified.")
+    set(CMAKE_BUILD_TYPE "Release" CACHE STRING "Choose the type of build." FORCE)
+    set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "" "Debug" "Release")
+else()
+    message(STATUS "Build type: ${CMAKE_BUILD_TYPE}")
+endif()
+
+# ##########################################
+# curand_examples directories
+# ##########################################
+
+# By default put binaries in build/bin (pre-install)
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+
+# Installation directories
+if (NOT CURAND_EXAMPLES_INSTALL_PREFIX)
+    set(CURAND_EXAMPLES_INSTALL_PREFIX curand_examples)
+endif()
+
+# ##########################################
+# Install examples
+# ##########################################
+
+if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+    set(CMAKE_INSTALL_PREFIX ${CMAKE_BINARY_DIR} CACHE PATH "" FORCE)
+endif()
 
 function(add_curand_example GROUP_TARGET EXAMPLE_NAME EXAMPLE_SOURCES)
-add_executable(${EXAMPLE_NAME} ${EXAMPLE_SOURCES})
-set_property(TARGET ${EXAMPLE_NAME} PROPERTY CUDA_ARCHITECTURES OFF)
-target_include_directories(${EXAMPLE_NAME}
-    PUBLIC
-        ${CUDA_INCLUDE_DIRS}
-)
-target_link_libraries(${EXAMPLE_NAME}
-    PUBLIC
-		CUDA::cudart
-		CUDA::curand
-)
-set_target_properties(${EXAMPLE_NAME} PROPERTIES
-    POSITION_INDEPENDENT_CODE ON
-)
+    add_executable(${EXAMPLE_NAME} ${EXAMPLE_SOURCES})
 
-# Install example
-install(
-    TARGETS ${EXAMPLE_NAME}
-    RUNTIME
-    DESTINATION ${curand_examples_BINARY_INSTALL_DIR}
-    PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ GROUP_EXECUTE GROUP_READ WORLD_EXECUTE WORLD_READ
-)
+    set_property(TARGET ${EXAMPLE_NAME} PROPERTY CUDA_ARCHITECTURES OFF)
 
-add_dependencies(${GROUP_TARGET} ${EXAMPLE_NAME})
+    target_include_directories(${EXAMPLE_NAME}
+        PRIVATE
+            "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../utils"
+        PUBLIC
+            ${CUDA_INCLUDE_DIRS}
+    )
+    target_link_libraries(${EXAMPLE_NAME}
+        PUBLIC
+            CUDA::cudart
+            CUDA::curand
+    )
+    set_target_properties(${EXAMPLE_NAME} PROPERTIES
+        POSITION_INDEPENDENT_CODE ON
+    )
+
+    # Install example
+    install(
+        TARGETS ${EXAMPLE_NAME}
+        RUNTIME
+        DESTINATION ${CURAND_EXAMPLES_INSTALL_PREFIX}/bin
+        PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ GROUP_EXECUTE GROUP_READ WORLD_EXECUTE WORLD_READ
+    )
+
+    if (TARGET ${GROUP_TARGET})
+        add_dependencies(${GROUP_TARGET} ${EXAMPLE_NAME})
+    endif()
 endfunction()
-
-add_custom_target(curand_examples)
